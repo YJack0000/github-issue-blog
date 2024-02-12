32bab557f47af93782e8d069764b95f01a294a90
YJack0000 <jackleagends@gmail.com>
2024-02-13 05:08:32 +0800

    feat: add global css and env configure root layout

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import Header from "@/components/Header"
+import "./globals.css"
+import Footer from "./components/Footer"
+
+const inter = Inter({ subsets: ["latin"] })
+
+const BLOG_NAME = process.env.BLOG_NAME as string
+const GITHUB_URL = process.env.GITHUB_URL as string
+const LINKEDIN_URL = process.env.LINKEDIN_URL as string
+
+export const metadata: Metadata = {
+    title: BLOG_NAME,
+    description: `A blog about web development and programming.`,
+}
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode
+}>) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <Header title={BLOG_NAME} />
+                <div className="min-h-screen max-w-6xl mx-auto flex flex-col">
+                    {children}
+                </div>
+                <Footer github={GITHUB_URL} linkedin={LINKEDIN_URL} />
+            </body>
+        </html>
+    )
+}
