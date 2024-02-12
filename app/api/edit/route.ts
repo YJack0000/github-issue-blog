38cadb2516ddd1f1ef2a16771788f9136c7d6e33
YJack0000 <jackleagends@gmail.com>
2024-02-13 05:07:11 +0800

    feat: create post without api

---
+++ app/api/edit/route.ts
@@ -0,0 +1,44 @@
+import { NextRequest, NextResponse } from "next/server"
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "@/api/auth/[...nextauth]/route"
+import { getAuthor } from "@/lib/github"
+
+const createPost = async (title: string, body: string, labels: string[]) => {
+    console.log("Creating post")
+}
+
+export async function POST(request: NextRequest) {
+    const session = await getServerSession(authOptions)
+
+    if (!session) {
+        return NextResponse.json(
+            { message: "Unauthorized" },
+            {
+                status: 403,
+            }
+        )
+    }
+
+    if (session.user.name !== (await getAuthor())) {
+        return NextResponse.json(
+            { message: "Unauthorized" },
+            {
+                status: 403,
+            }
+        )
+    }
+
+    const form = (await request.json()) as any
+    try {
+        await createPost(form.title, form.body, form.labels)
+    } catch (error) {
+        return NextResponse.json(
+            { message: "Error creating post" },
+            {
+                status: 500,
+            }
+        )
+    }
+
+    return NextResponse.json({ message: "Success" })
+}
---
+++ app/components/post/PostForm.tsx
@@ -0,0 +1,125 @@
+"use client"
+import { useState } from "react"
+import dynamic from "next/dynamic"
+const Select = dynamic(() => import("react-select"), { ssr: false }) // ensure react-select is not server rendered, prevent hydration warning
+
+export interface PostFormProps {
+    header: string
+    title?: string
+    id?: number
+    tags?: string[]
+    body?: string
+}
+
+type PostFormState = {
+    id: number | undefined
+    title: string | undefined
+    tags: string[]
+    body: string | undefined
+}
+
+export default function PostForm({
+    header,
+    title,
+    id,
+    tags,
+    body,
+}: PostFormProps) {
+    const [form, setForm] = useState<PostFormState>({
+        id: id,
+        title: title,
+        tags: tags || [],
+        body: body,
+    })
+
+    const updateForm = (name: string, value: string | string[]) => {
+        setForm((prevState: any) => ({
+            ...prevState,
+            [name]: value,
+        }))
+    }
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault()
+        try {
+            const response = await fetch("/api/edit", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(form),
+            })
+            console.log(response.json())
+        } catch (error) {
+            console.error("Error:", error)
+        }
+    }
+
+    return (
+        <>
+            <div className="">
+                <button
+                    className="btn btn-primary"
+                    onClick={() =>
+                        document.getElementById("post_form")?.showModal()
+                    }
+                >
+                    {header}
+                </button>
+            </div>
+            <dialog id="post_form" className="modal">
+                <div className="modal-box w-11/12 modal-middle max-w-5xl">
+                    <h2 className="font-bold text-lg my-4">
+                        {header} #{id}
+                    </h2>
+                    <form
+                        onSubmit={handleSubmit}
+                        className="form-control w-full [&>*]:mb-2"
+                    >
+                        <input
+                            type="text"
+                            placeholder="文章標題"
+                            value={form.title}
+                            className="input input-bordered w-full"
+                            onChange={(e) =>
+                                updateForm("title", e.target.value)
+                            }
+                        />
+                        <Select
+                            instanceId={"select-tags"} // some unique id to prevent hydrate warning
+                            isMulti
+                            className="w-full"
+                            defaultValue={() => {
+                                return form.tags.map((t) => {
+                                    return { value: t, label: t }
+                                })
+                            }}
+                            options={[
+                                { value: "test", label: "test" },
+                                { value: "tt", label: "tt" },
+                            ]}
+                            onChange={(selected: any) =>
+                                updateForm(
+                                    "tags",
+                                    selected.map((s: any) => s.value)
+                                )
+                            }
+                        ></Select>
+                        <textarea
+                            placeholder="文章內容"
+                            className="textarea textarea-bordered w-full h-96"
+                            value={form.body}
+                            onChange={(e) => updateForm("body", e.target.value)}
+                        />
+                        <button type="submit" className="btn btn-primary">
+                            送出
+                        </button>
+                    </form>
+                </div>
+                <form method="dialog" className="modal-backdrop">
+                    <button>關閉</button>
+                </form>
+            </dialog>
+        </>
+    )
+}
