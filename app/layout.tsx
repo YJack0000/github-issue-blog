1e1b45e1adc383a75e76e2def61a1949ddfb7e42
YJack0000 <jackleagends@gmail.com>
2024-03-02 20:01:04 +0800

    fix: import @

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import Header from "@/components/Header"
 import "./globals.css"
-import Footer from "./components/Footer"
+import Footer from "@/components/Footer"
 
 const inter = Inter({ subsets: ["latin"] })
 
