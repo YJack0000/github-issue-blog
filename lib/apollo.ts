0993d7b2a9f0aa7e3cc2a28488fdcfb7311495d6
YJack0000 <jackleagends@gmail.com>
2024-04-08 11:40:16 +0800

    feat: fix in memory cache conflict problem by using customized Id

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,11 +1,24 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 
+import { defaultDataIdFromObject } from "@apollo/client"
+
 let apolloClient: ApolloClient<any> | null = null
 
 const createApolloClient = () =>
     new ApolloClient({
         uri: "https://api.github.com/graphql",
-        cache: new InMemoryCache(),
+        cache: new InMemoryCache({
+            dataIdFromObject(responseObject) {
+                switch (responseObject.__typename) {
+                    case "Repository":
+                        // 假設每個Repository對象都有owner和name字段
+                        return `Repository:${responseObject.owner}:${responseObject.name}`
+                    // 你可以為其他類型添加更多的規則
+                    default:
+                        return defaultDataIdFromObject(responseObject)
+                }
+            },
+        }),
     })
 
 export const getClient = () => {
